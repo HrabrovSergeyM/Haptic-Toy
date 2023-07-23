db43d66ca5442b2810ee0cd4b287292e012bdbea
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-07-23 20:40:29 +0300

    Initial Commit

---
+++ Haptic Toy/Haptic_ToyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Haptic_ToyApp.swift
+//  Haptic Toy
+//
+//  Created by Sergey Hrabrov on 23.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Haptic_ToyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
