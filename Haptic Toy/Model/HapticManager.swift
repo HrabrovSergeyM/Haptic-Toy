79a081aae93a684eafea961c1ae9d402baaefbb8
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-08-04 08:07:50 +0300

    main: First implementation of ToggleView, fixed HapticManager

--- Haptic Toy/Model/HapticManager.swift
+++ Haptic Toy/Model/HapticManager.swift
@@ -29,40 +29,26 @@ class HapticManager {
         guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
         do {
             self.engine = try CHHapticEngine()
+            engine?.stoppedHandler = { reason in
+                print("Engine stopped with reason: \(reason.rawValue)")
+                if reason == .audioSessionInterrupt {
+                    self.restartEngine()
+                }
+            }
             try engine?.start()
         } catch {
             print("There was an error creating the engine: \(error.localizedDescription)")
         }
     }
-    
-//    static func startHaptics() {
-//        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-//        var events = [CHHapticEvent]()
-//
-//        for i in stride(from: 0, to: 1, by: 0.1) {
-//            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
-//            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
-//            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
-//            events.append(event)
-//        }
-//
-//        for i in stride(from: 0, to: 1, by: 0.1) {
-//            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
-//            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
-//            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
-//            events.append(event)
-//        }
-//
-//        do {
-//            let pattern = try CHHapticPattern(events: events, parameters: [])
-//            self.player = try engine?.makeAdvancedPlayer(with: pattern)
-//            try player?.start(atTime: 0)
-//            isPlaying = true
-//            repeatHaptics()
-//        } catch {
-//            print("Failed to play pattern: \(error.localizedDescription).")
-//        }
-//    }
+
+    private static func restartEngine() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        do {
+            try self.engine?.start()
+        } catch {
+            print("Failed to restart the engine: \(error)")
+        }
+    }
     
     static func startHaptics(intensityValue: Float? = nil) {
         guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
@@ -99,6 +85,22 @@ class HapticManager {
             try self.player?.stop(atTime: 0)
         } catch {
             print("Failed to stop haptics: \(error.localizedDescription)")
+        }
+    }
+    
+    static func playHapticWithIntensity(_ intensity: Float, sharpness: Float) {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        let intensityParameter = CHHapticEventParameter(parameterID: .hapticIntensity, value: intensity)
+        let sharpnessParameter = CHHapticEventParameter(parameterID: .hapticSharpness, value: sharpness)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensityParameter, sharpnessParameter], relativeTime: 0)
+
+        do {
+            let pattern = try CHHapticPattern(events: [event], parameters: [])
+            let player = try engine?.makeAdvancedPlayer(with: pattern)
+            try player?.start(atTime: CHHapticTimeImmediate)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
         }
     }
     
--- Haptic Toy/View/ToggleView.swift
+++ Haptic Toy/View/ToggleView.swift
@@ -9,8 +9,6 @@ import SwiftUI
 
 struct ToggleView: View {
     
-    @State private var buttonToggle = false
-    @State private var switchToggle = false
     @State var showHelp: Bool = false
     
     var body: some View {
@@ -18,27 +16,76 @@ struct ToggleView: View {
             
             Color(UIColor.tertiarySystemBackground).ignoresSafeArea()
             
-            VStack(spacing: 40) {
-                Toggle("Toggle", isOn: $buttonToggle)
-                    .toggleStyle(CheckboxStyle())
+            VStack(spacing: 20) {
+                VStack(spacing: 20) {
+                    Text("Soft")
+                        .font(Font.system(size: 28, weight: .thin, design: .rounded))
+                    
+                    HStack(spacing: 20) {
+                        ForEach(0..<5) { index in
+                            Button(action: {
+                                let intensityValue = 0.4 + (Float(index) * 0.04)
+                                let sharpnessValue = 0.5 + (Float(index) * 0.04)
+                                HapticManager.playHapticWithIntensity(intensityValue, sharpness: sharpnessValue)
+                            }) {
+                                RoundedRectangle(cornerRadius: 5)
+                                    .fill(Color("ColorGray").opacity(Double(index + 1) * 0.03 + 0.1))
+                                    .shadow(color: .primary.opacity(0.4), radius: 4, x: 0, y: 0)
+                                    .frame(width: 50, height: 50)
+                            }
+                            
+                        }
+                    }
+                }
                 
-                Toggle("Toggle", isOn: $switchToggle)
-                    .toggleStyle(.switch)
-                    .labelsHidden()
+                VStack(spacing: 20) {
+                    Text("Medium")
+                        .font(Font.system(size: 28, weight: .medium, design: .rounded))
+                    HStack(spacing: 20) {
+                        ForEach(0..<5) { index in
+                            Button(action: {
+                                let intensityValue = 0.6 + (Float(index) * 0.04)
+                                let sharpnessValue = 0.7 + (Float(index) * 0.04)
+                                HapticManager.playHapticWithIntensity(intensityValue, sharpness: sharpnessValue)
+                            }) {
+                                RoundedRectangle(cornerRadius: 5)
+                                    .fill(Color("ColorGray").opacity(Double(index + 1) * 0.03 + 0.3))
+                                    .shadow(color: .primary.opacity(0.4), radius: 4, x: 0, y: 0)
+                                    .frame(width: 50, height: 50)
+                            }
+                        }
+                    }
+                }
                 
-            } // VStack
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    ToolbarHelpButton(showHelp: $showHelp)
+                VStack(spacing: 20) {
+                    Text("Heavy")
+                        .font(Font.system(size: 28, weight: .heavy, design: .rounded))
+                    
+                    HStack(spacing: 20) {
+                        ForEach(0..<5) { index in
+                            Button(action: {
+                                let intensityValue = 0.8 + (Float(index) * 0.06)
+                                let sharpnessValue = 0.9 + (Float(index) * 0.02)
+                                HapticManager.playHapticWithIntensity(intensityValue, sharpness: sharpnessValue)
+                            }) {
+                                RoundedRectangle(cornerRadius: 5)
+                                    .fill(Color("ColorGray").opacity(Double(index + 1) * 0.03 + 0.6))
+//                                    .fill(Color("ColorGray"))
+                                    .shadow(color: .primary.opacity(0.4), radius: 4, x: 0, y: 0)
+                                    .frame(width: 50, height: 50)
+                            }
+                        }
+                    }
                 }
             }
-            .onAppear {
-                showHelp = !UserDefaults.standard.bool(forKey: "ToggleView")
-            }
-            .sheet(isPresented: $showHelp, content: {
-                HelpView(helpText: NSLocalizedString("helpViewToggle", comment: ""), screenKey: "ToggleView", isPresented: $showHelp)
-            })
         }
+        .onAppear {
+            showHelp = !UserDefaults.standard.bool(forKey: "ToggleView")
+            HapticManager.prepareHaptics()
+        }
+        .sheet(isPresented: $showHelp, content: {
+            HelpView(helpText: NSLocalizedString("helpViewToggle", comment: ""), screenKey: "ToggleView", isPresented: $showHelp)
+        })
     }
 }
 
