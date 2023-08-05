93b2aa2e8f708cc1140fc24f0c1e67e13a98b1b1
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-08-05 10:37:46 +0300

    HT-1: Added alternate icon, refactoring

--- Haptic Toy/View/SettingsView/SettingsView.swift
+++ Haptic Toy/View/SettingsView/SettingsView.swift
@@ -3,7 +3,6 @@
 //  Haptic Toy
 //
 //  Created by Sergey Hrabrov on 04.08.2023.
-//"moon.circle.fill" : "moon.circle"
 
 import SwiftUI
 
@@ -12,52 +11,18 @@ struct SettingsView: View {
     @State var isEng: Bool = LanguageManager.language == "en"
     @State var isDarkIcon: Bool = false
     @Binding var isShowingSettings: Bool
-    
+
     var body: some View {
         ZStack {
             Color(UIColor.systemGray6).ignoresSafeArea()
-            
-            VStack(spacing: 20) {
+
+            VStack(alignment: .center, spacing: 30) {
+               
                 
-                HStack {
-                    Image(systemName: "sun.max")
-                        .font(.title2)
-                    Toggle("Dark Mode", isOn: $isDarkMode)
-                        .labelsHidden()
-                        .toggleStyle(SwitchToggleStyle())
-                    //                        .onChange(of: isDarkMode) { value in
-                    //                            isDarkMode.toggle()
-                    //                        }
-                    Image(systemName: "moon.circle.fill")
-                        .font(.title2)
-                }
+                DarkModeToggleView(isDarkMode: $isDarkMode)
+                LanguageToggleView(isEng: $isEng)
+                IconToggleView(isDarkIcon: $isDarkIcon)
                 
-                HStack {
-                    Text("rus")
-                        .font(Font.system(size: 24, weight: .thin, design: .rounded))
-                    Toggle("Dark Mode", isOn: $isEng)
-                        .labelsHidden()
-                        .toggleStyle(SwitchToggleStyle())
-                        .onChange(of: isEng) { value in
-                            LanguageManager.language = value ? "en" : "ru"
-                        }
-                    Text("eng")
-                        .font(Font.system(size: 24, weight: .thin, design: .rounded))
-                }
-                
-                HStack {
-                    Image("settingsDarkIcon")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 60, height: 60)
-                    Toggle("Dark Mode", isOn: $isDarkIcon)
-                        .labelsHidden()
-                        .toggleStyle(SwitchToggleStyle())
-                    Image("settingsLightIcon")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 60, height: 60)
-                }
                 Button {
                     withAnimation {
                         isShowingSettings = false
@@ -65,14 +30,91 @@ struct SettingsView: View {
                 } label: {
                     Text("Close")
                 }
-                
             }
-            .toolbar {
-                ToolbarItem {
-                    
-                }
+        }
+    }
+
+    private func changeAppIcon(isDarkIcon: Bool) {
+        UIApplication.shared.setAlternateIconName(isDarkIcon ? "AppDarkIcon" : nil) { error in
+            if let error = error {
+                print(error.localizedDescription)
             }
+        }
+    }
+
+    private func setLanguage(isEng: Bool) {
+        LanguageManager.language = isEng ? "en" : "ru"
+    }
+}
+
+struct DarkModeToggleView: View {
+    @Binding var isDarkMode: Bool
+
+    var body: some View {
+        HStack(alignment: .center, spacing: 20) {
+           
+                Image(systemName: isDarkMode ? "sun.min" : "sun.max")
+                    .font(.title)
+                    .frame(width: 42, height: 42)
             
+            Toggle("Dark Mode", isOn: $isDarkMode)
+                .labelsHidden()
+                .toggleStyle(SwitchToggleStyle())
+            Image(systemName: isDarkMode ? "moon.stars" : "moon.zzz")
+                .font(.title)
+                .frame(width: 42, height: 42)
+        }
+    }
+}
+
+struct LanguageToggleView: View {
+    @Binding var isEng: Bool
+
+    var body: some View {
+        HStack(alignment: .center, spacing: 20) {
+            Text("rus")
+                .font(Font.system(size: 24, weight: .thin, design: .rounded))
+                .frame(width: 42, height: 42)
+            Toggle("Dark Mode", isOn: $isEng)
+                .labelsHidden()
+                .toggleStyle(SwitchToggleStyle())
+                .onChange(of: isEng) { value in
+                    LanguageManager.language = value ? "en" : "ru"
+                }
+            Text("eng")
+                .font(Font.system(size: 24, weight: .thin, design: .rounded))
+                .frame(width: 42, height: 42)
+        }
+    }
+}
+
+struct IconToggleView: View {
+    @Binding var isDarkIcon: Bool
+
+    var body: some View {
+        HStack(alignment: .center, spacing: 20) {
+            Image("settingsLightIcon")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 60, height: 60)
+            Toggle("Dark Mode", isOn: $isDarkIcon)
+                .labelsHidden()
+                .toggleStyle(SwitchToggleStyle())
+                .onChange(of: isDarkIcon) { newValue in
+                    changeAppIcon(isDarkIcon: newValue)
+                }
+            Image("settingsDarkIcon")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 60, height: 60)
+        }
+    }
+
+    private func changeAppIcon(isDarkIcon: Bool) {
+        UIApplication.shared.setAlternateIconName(isDarkIcon ? "AppDarkIcon" : nil) { error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
         }
     }
 }
