43dca4592defdaff0b890351e3177618b83c7db1
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-08-08 09:20:31 +0300

    HT-4: First implementation with CoreMotion

--- Haptic Toy/View/ButtonsView/ButtonsSection.swift
+++ Haptic Toy/View/ButtonsView/ButtonsSection.swift
@@ -6,22 +6,25 @@
 //
 
 import SwiftUI
+import CoreMotion
 
 struct ButtonsSection: View {
     
     @Environment(\.colorScheme) var colorScheme
     @Binding var isStackVisible: Bool
-     var intensity: Float
-     var baseOpacity: Double
-     var offsetOpacity: Double
-     var title: String
-     var yOffset: CGFloat
+    @Binding var motion: CMDeviceMotion?
+    let motionManager: CMMotionManager
+    var intensity: Float
+    var baseOpacity: Double
+    var offsetOpacity: Double
+    var title: String
+    var yOffset: CGFloat
     
-    private func getColor(forIndex index: Int, baseOpacity: Double, offsetOpacity: Double) -> Color {
+    private func getColor(color: Color, forIndex index: Int, baseOpacity: Double, offsetOpacity: Double) -> Color {
         let opacity = colorScheme == .light ?
-            baseOpacity + (Double(index + 1) * offsetOpacity) :
-            baseOpacity + (Double(1 - index) * offsetOpacity)
-        return Color("ColorGray").opacity(opacity)
+        baseOpacity + (Double(index + 1) * offsetOpacity) :
+        baseOpacity + (Double(1 - index) * offsetOpacity)
+        return color.opacity(opacity)
     }
     
     var body: some View {
@@ -34,11 +37,45 @@ struct ButtonsSection: View {
                         let sharpnessValue = 1.0 - (Float(index) * 0.15)
                         HapticManager.playHapticWithIntensity(intensity, sharpness: sharpnessValue)
                     }) {
-                        RoundedRectangle(cornerRadius: 5)
-                            .fill(getColor(forIndex: index, baseOpacity: baseOpacity, offsetOpacity: offsetOpacity))
-                            .shadow(color: .primary.opacity(0.4), radius: 4, x: 0, y: 0)
-                            .frame(width: 50, height: 50)
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 5)
+//                                .fill(Color(UIColor.tertiarySystemBackground))
+                                .fill(getColor(color: Color("ColorGray"), forIndex: index, baseOpacity: baseOpacity, offsetOpacity: offsetOpacity))
+                                .shadow(color: .black.opacity(0.1), radius: 4, x: 0, y: 0)
+                                .frame(width: 50, height: 50)
+                                
+                                .offset(
+                                    x: motion != nil ? CGFloat(motion!.gravity.x * 15) : 0,
+                                    y: motion != nil ? CGFloat(-motion!.gravity.y * 15) : 0
+                                )
+                                //                    .rotation3DEffect(motion != nil ? .degrees(Double(motion!.attitude.pitch) * 5 / .pi) : .degrees(0))
+                                .rotation3DEffect(
+                                    motion != nil ? .degrees(Double(motion!.attitude.pitch) * 3 / .pi) : .degrees(0),
+                                    axis: (
+                                        x: motion != nil ? -motion!.gravity.y : 0,
+                                        y: motion != nil ? motion!.gravity.x : 0,
+                                        z: 0)
+                                )
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(getColor(color: Color("ColorGray"), forIndex: index, baseOpacity: baseOpacity, offsetOpacity: offsetOpacity))
+                                .shadow(color: .primary.opacity(0.4), radius: 4, x: 0, y: 0)
+                                .frame(width: 50, height: 50)
+//                                .blendMode(.overlay)
+                                .offset(
+                                    x: motion != nil ? CGFloat(motion!.gravity.x * 20) : 0,
+                                    y: motion != nil ? CGFloat(-motion!.gravity.y * 20) : 0
+                                )
+                                //                    .rotation3DEffect(motion != nil ? .degrees(Double(motion!.attitude.pitch) * 5 / .pi) : .degrees(0))
+                                .rotation3DEffect(
+                                    motion != nil ? .degrees(Double(motion!.attitude.pitch) * 5 / .pi) : .degrees(0),
+                                    axis: (
+                                        x: motion != nil ? -motion!.gravity.y : 0,
+                                        y: motion != nil ? motion!.gravity.x : 0,
+                                        z: 0)
+                                )
+                        }
                     }
+                  
                 }
             }
         }
@@ -50,6 +87,6 @@ struct ButtonsSection: View {
 
 struct ButtonsSection_Previews: PreviewProvider {
     static var previews: some View {
-        ButtonsSection(isStackVisible: .constant(true), intensity: (0.5), baseOpacity: (0.5), offsetOpacity: (0.5), title: ("Soft"), yOffset: (40))
+        ButtonsSection(isStackVisible: .constant(true), motion: .constant(.none), motionManager: CMMotionManager(), intensity: (0.5), baseOpacity: (0.1), offsetOpacity: (0.1), title: ("Soft"), yOffset: (40))
     }
 }
--- Haptic Toy/View/ButtonsView/ButtonsView.swift
+++ Haptic Toy/View/ButtonsView/ButtonsView.swift
@@ -7,19 +7,23 @@
 // TODO: refactoring
 
 import SwiftUI
+import CoreMotion
 
 struct ButtonsView: View {
     @Environment(\.colorScheme) var colorScheme
     @State var showHelp: Bool = false
     @State var isStackVisible: Bool = false
+    
+    let motionManager = CMMotionManager()
+    @State var motion: CMDeviceMotion? = nil
 
     var body: some View {
         ZStack {
             Color(UIColor.tertiarySystemBackground).ignoresSafeArea()
             VStack(spacing: 20) {
-                ButtonsSection(isStackVisible: $isStackVisible, intensity: 0.6, baseOpacity: 0.1, offsetOpacity: 0.03, title: "Soft", yOffset: -40)
-                ButtonsSection(isStackVisible: $isStackVisible, intensity: 0.8, baseOpacity: 0.3, offsetOpacity: 0.03, title: "Medium", yOffset: -60)
-                ButtonsSection(isStackVisible: $isStackVisible, intensity: 1.0, baseOpacity: 0.5, offsetOpacity: 0.03, title: "Heavy", yOffset: -80)
+                ButtonsSection(isStackVisible: $isStackVisible, motion: $motion, motionManager: motionManager, intensity: 0.6, baseOpacity: 0.1, offsetOpacity: 0.03, title: "Soft", yOffset: -40)
+                ButtonsSection(isStackVisible: $isStackVisible, motion: $motion, motionManager: motionManager, intensity: 0.8, baseOpacity: 0.3, offsetOpacity: 0.03, title: "Medium", yOffset: -60)
+                ButtonsSection(isStackVisible: $isStackVisible, motion: $motion, motionManager: motionManager, intensity: 1.0, baseOpacity: 0.5, offsetOpacity: 0.03, title: "Heavy", yOffset: -80)
             }
         }
         .toolbar {
@@ -28,14 +32,22 @@ struct ButtonsView: View {
             }
         }
         .onAppear {
-            showHelp = !UserDefaults.standard.bool(forKey: "ToggleView")
+            showHelp = !UserDefaults.standard.bool(forKey: "ButtonsView")
             withAnimation(.easeInOut(duration: 1)) {
                 isStackVisible = true
+            }
+            if motionManager.isDeviceMotionAvailable {
+                self.motionManager.deviceMotionUpdateInterval = 1.0 / 60.0
+                self.motionManager.startDeviceMotionUpdates(to: OperationQueue.main) { (data, error) in
+                    if let validData = data {
+                        self.motion = validData
+                    }
+                }
             }
             HapticManager.prepareHaptics()
         }
         .sheet(isPresented: $showHelp, content: {
-            HelpView(helpText: "helpViewToggle", screenKey: "ToggleView", isPresented: $showHelp)
+            HelpView(helpText: "helpViewToggle", screenKey: "ButtonsView", isPresented: $showHelp)
         })
     }
 }
