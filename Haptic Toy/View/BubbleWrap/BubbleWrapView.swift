44a2eae0953c459e5ca6802821a46b13ce4a80c5
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-08-10 11:07:00 +0300

    HT-6: Added Single Tap / Slice mode

--- Haptic Toy/View/BubbleWrap/BubbleWrapView.swift
+++ Haptic Toy/View/BubbleWrap/BubbleWrapView.swift
@@ -10,81 +10,94 @@ import SwiftUI
 struct BubbleWrapView: View {
     @State var showHelp: Bool = false
     @State var restartKey: Bool = false
-//    @State var split: Bool = false
+    @State var gameMode: Bool = true
+    //    @State var split: Bool = false
     @State private var displayMode: DisplayMode = .standard
     
     var body: some View {
         ZStack {
             
             Color(UIColor.tertiarySystemBackground).ignoresSafeArea()
-            
-            
-                //                VStack {
-                //                    HStack {
-                //
-                //                    }
-                //                BubbleView(restartKey: $restartKey, displayMode: $displayMode)
-            GeometryReader { geometry in
-                SpriteKitView(sceneSize: geometry.size, displayMode: $displayMode, restartKey: $restartKey)
+            VStack {
+                if gameMode {
+                    
+                    BubbleView(restartKey: $restartKey, displayMode: $displayMode)
+                    
+                } else {
+                    GeometryReader { geometry in
+                        SpriteKitView(sceneSize: geometry.size, displayMode: $displayMode, restartKey: $restartKey)
+                    }
+                }
+                
+                
             }
-                        
-//                }
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        ToolbarHelpButton(showHelp: $showHelp)
-                    }
+            
+            
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    ToolbarHelpButton(showHelp: $showHelp)
                 }
-    //            .toolbar {
-    //                ToolbarItem(placement: .navigationBarTrailing) {
-    //                    BubbleMenuButtonView()
-    //                }
-    //            }
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button {
-                            withAnimation(.spring()) {
-                                restartKey.toggle()
-                                HapticManager.notification(type: .success)
-                            }
-                        } label: {
-                            Image(systemName: "arrow.triangle.2.circlepath")
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        gameMode.toggle()
+                    } label: {
+                        if gameMode {
+                            Image(systemName: "hand.tap")
+                        } else {
+                            Image(systemName: "hand.draw")
+                        }
+                    }
+
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        withAnimation(.spring()) {
+                            restartKey.toggle()
+                            HapticManager.notification(type: .success)
+                        }
+                    } label: {
+                        Image(systemName: "arrow.triangle.2.circlepath")
+                    }
+                    
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        withAnimation(.spring()) {
+                            displayMode.toggle()
+                        }
+                        switch displayMode {
+                        case .standard:
+                            HapticManager.impact(style: .light)
+                        case .extended:
+                            HapticManager.impact(style: .medium)
+                        case .maximum:
+                            HapticManager.impact(style: .heavy)
                         }
                         
-                    }
-                }
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button {
-                            withAnimation(.spring()) {
-                                displayMode.toggle()
-                            }
-                            switch displayMode {
-                            case .standard:
-                                HapticManager.impact(style: .light)
-                            case .extended:
-                                HapticManager.impact(style: .medium)
-                            case .maximum:
-                                HapticManager.impact(style: .heavy)
-                            }
-                            
-                        } label: {
-                            switch displayMode {
-                            case .standard:
-                                Image(systemName: "rectangle.split.2x2.fill")
-                            case .extended:
-                                Image(systemName: "rectangle.split.3x3.fill")
-                            case .maximum:
-                                Image(systemName: "square.split.2x1.fill")
-                            }
+                    } label: {
+                        switch displayMode {
+                        case .standard:
+                            Image(systemName: "rectangle.split.2x2.fill")
+                        case .extended:
+                            Image(systemName: "rectangle.split.3x3.fill")
+                        case .maximum:
+                            Image(systemName: "square.split.2x1.fill")
                         }
-                        
                     }
+                    
                 }
-                .onAppear {
-                    showHelp = !UserDefaults.standard.bool(forKey: "BubbleWrapView")
-                }
-                .sheet(isPresented: $showHelp, content: {
-                    HelpView(helpText: "helpViewBubble", screenKey: "BubbleWrapView", isPresented: $showHelp)
+            }
+            .onAppear {
+                showHelp = !UserDefaults.standard.bool(forKey: "BubbleWrapView")
+            }
+            .sheet(isPresented: $showHelp, content: {
+                HelpView(helpText: "helpViewBubble", screenKey: "BubbleWrapView", isPresented: $showHelp)
             })
             
         }
